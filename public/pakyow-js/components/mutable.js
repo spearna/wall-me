c9157edc5ebc897e14d551bd4e9ded6cdbebe0fc
Chad Taylor <taylor.thomas.c@gmail.com>
2016-05-18 17:32:52 -0500

    Update ring.js

--- public/pakyow-js/components/mutable.js
+++ public/pakyow-js/components/mutable.js
@@ -1,19 +1,27 @@
 pw.component.register('mutable', function (view, config) {
   this.mutation = function (mutation) {
+    // no socket, just submit the form
+    if (!window.socket) {
+      view.node.submit();
+      return;
+    }
+
     var datum = pw.util.dup(mutation);
     delete datum.__nested;
     delete datum.scope;
     delete datum.id;
 
-    var input = {};
-    input[mutation.scope] = datum;
-
     var message = {
-      action: 'call_route',
-      input: input
+      action: 'call-route'
     };
 
     if (view.node.tagName === 'FORM') {
+      if (view.node.querySelector('input[type="file"]')) {
+        // file uploads over websocket are not supported
+        view.node.submit();
+        return;
+      }
+
       var method;
       var $methodOverride = view.node.querySelector('input[name="_method"]');
       if ($methodOverride) {
@@ -24,31 +32,39 @@ pw.component.register('mutable', function (view, config) {
 
       message.method = method;
       message.uri = view.node.getAttribute('action');
+      message.input = pw.node.serialize(view.node);
     } else {
       //TODO deduce uri / method
+
+      var input = {};
+      input[mutation.scope] = datum;
+      message.input = input;
     }
 
     var self = this;
     window.socket.send(message, function (res) {
-      if (res.status === 302 && res.headers.Location !== window.location.pathname) {
+      if (res.status === 302) {
         var dest = res.headers.Location;
-        //TODO trigger a response:redirect instead and let navigator subscribe
-        history.pushState({ uri: dest }, dest, dest);
-        return;
+
+        if (dest == window.location.pathname && (!window.context || window.context.name !== 'default')) {
+          history.pushState({ uri: dest }, dest, dest);
+        } else {
+          //TODO trigger a response:redirect instead and let navigator subscribe
+          history.pushState({ uri: dest }, dest, dest);
+        }
       } else if (res.status === 400) {
         // bad request
+        pw.component.broadcast('response:received', { response: res });
+        return;
       } else {
         self.state.rollback();
       }
 
-      pw.component.push({
-        channel: 'response:received',
-        payload: {
-          response: res
-        }
-      });
+      pw.component.broadcast('response:received', { response: res });
 
-      self.revert();
+      if (config.revert !== 'false') {
+        self.revert();
+      }
     });
   }
 });
