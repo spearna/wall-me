dbe0bf3a8738ce53e8358f9924ab1716099177e3
Kyle Newman <kyle@skylenewman.com>
2016-06-28 17:03:09 -0500

    Added binding for image; commented bindings code

--- app/lib/bindings.rb
+++ app/lib/bindings.rb
@@ -7,7 +7,27 @@ Pakyow::App.bindings do
       ["Look around",
        "Join a team"
       ].map { |x| [x.to_sym, x] }
-    end
-  end
+    end # options :objective do
 
-end
+    binding(:imgurl) do
+      # Default the image to superman
+      url = '/images/superman_profile.jpg'
+      # Title for mouseover
+      title = 'Profile photo'
+      unless bindable.imgurl.nil?
+        url = bindable.imgurl
+      end
+      unless bindable.firstName.nil? || bindable.lastName.nil?
+        title = "#{title} of #{bindable.firstName} #{bindable.lastName}"
+      end
+      {
+        # TODO: Uncomment when upgrade ring.js
+        # :class => ["media-object dp img-circle"],
+        :src => url,
+        :title => title
+      }
+    end # binding(:imgurl) do
+
+  end # scope :profile do
+
+end # Pakyow::App.bindings do
